cf8a0969207deb06b91de717ac238773141b70ab
Cristoir <cmullins017@gmail.com>
2018-10-25 20:48:08 +0100

    Add files via upload

---
+++ src/q1/assignment1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+
+int main(int arg, char* argc[]){
+
+  printf("Hello assignment1.\n");
+  
+  int i = sizeof(i);
+  int* ip = sizeof(ip);
+  long l = sizeof(l);
+  double* d = sizeof(d);
+  char** c = sizeof(c);
+
+
+  printf("%d\n", i);
+  printf("%d\n", ip);
+  printf("%d\n", l);
+  printf("%d\n", d);
+  printf("%d\n", c);
+  
+
+  
+  
+
+  return 0;
+
+}
